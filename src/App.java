2c122890b0db52be96b2b9295f5f821f077ecc40
te22e-eriksson-adrian <adrian.eriksson@elev.ga.ntig.se>
2023-10-16 09:40:03 +0200

    gjorde klart del a

--- src/App.java
+++ src/App.java
@@ -1,5 +1,32 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //int litetTal = 10;
+
+        /* if (litetTal<1000)
+        {
+            int stortTal = 1000;
+            System.out.println(litetTal+" är mindre än "+stortTal);
+        }
+        else
+        {
+            System.out.println(litetTal+" är mindre än "+stortTal);
+        } */
+
+        System.out.println("Varför fungerar inte koden?");
+        System.out.println("Svar: Eftersom villkoret inte gäller så körs inte de instruktionerna som är ovanför 'else' detta inkluderar introduktionen av 'stortTal'.");
+        System.out.println("Hur kan du få koden att fungera?");
+        System.out.println("Svar: Genom att ta ut introduktionen av variabeln 'stortTal' och placera den tillsammans med 'litetTal'. (Se lösning nedan.)");
+
+        int litetTal = 10;
+        int stortTal = 1000;
+
+        if (litetTal<1000)
+        {
+            System.out.println(litetTal+" är mindre än "+stortTal);
+        }
+        else
+        {
+            System.out.println(litetTal+" är mindre än "+stortTal);
+        }
     }
 }
